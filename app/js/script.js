5f826f42807b01df73f61216d978df0b3f5507fc
joaoarthurbandeira <66695557+joaoarthurbandeira@users.noreply.github.com>
2023-03-30 11:48:45 -0300

    Project-EasyBank-LP-v1-part1to6

---
+++ app/js/script.js
@@ -0,0 +1,27 @@
+const btnHamburguer = document.querySelector("#btnHamburguer");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+
+btnHamburguer.addEventListener("click", function () {
+  console.log("click hamburguer");
+
+  if (header.classList.contains("open")) {
+    //Close Hamburguer Menu
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
+  } else {
+    //Open Hamburguer Menu
+    body.classList.add("noscroll");
+    header.classList.add("open");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
+  }
+});
